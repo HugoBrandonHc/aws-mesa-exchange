a3997d96c388c2c81a95d3df48847e1fb214f323
HugoBrandonHc <hugobrandon17@gmail.com>
2024-11-24 21:28:47 -0500

    actualizacion v4

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports'; // Asegúrate de que aws-exports.js esté configurado correctamente
-
+import awsExports from './aws-exports'; 
 // Configuración global de Amplify
 Amplify.configure(awsExports);
 
